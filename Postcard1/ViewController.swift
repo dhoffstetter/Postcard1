10b4094130505ad433612f5aee2a9f89f3d8794e
Diane Hoffstetter <dhoffstetter@mac.com>
2015-07-04 11:55:08 -0700

    added nameLabel

--- Postcard1/ViewController.swift
+++ Postcard1/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
   
   @IBOutlet weak var messageLabel: UILabel!
+  @IBOutlet weak var nameLabel: UILabel!
   @IBOutlet weak var enterNameTextField: UITextField!
   @IBOutlet weak var enterMessageTextField: UITextField!
   @IBOutlet weak var mailButton: UIButton!
@@ -32,9 +33,15 @@ class ViewController: UIViewController {
     messageLabel.hidden = false
     messageLabel.text = enterMessageTextField.text
     messageLabel.textColor = UIColor.redColor()
-    
     enterMessageTextField.text = ""
+    
+    nameLabel.hidden = false
+    nameLabel.text = enterNameTextField.text
+    nameLabel.textColor = UIColor.blueColor()
+    enterNameTextField.text = ""
+    
     enterMessageTextField.resignFirstResponder()
+    enterNameTextField.resignFirstResponder()
     
     mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
     
